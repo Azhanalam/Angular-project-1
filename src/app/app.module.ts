c5af18c8ac9353c336515b47773156d1b9fbc5ab
AZ20054228 <azhan.alam@wipro.com>
2019-04-05 19:26:25 +0530

    Added Login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule, MatInputModule } from '@angular/material';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule }   from '@angular/forms';
+import { MatDividerModule } from '@angular/material/divider';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
+    MatDividerModule,
+    MatInputModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule
   ],
