da1829d296af9775eb63cf4af4c9edeb5bbbe22e
AZ20054228 <azhan.alam@wipro.com>
2019-04-08 17:51:44 +0530

    Modified Login page, Added welcome and Requested form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-
+import { WelcomeComponent } from './welcome/welcome.component';
 import {HeaderComponent} from './header/header.component';
 import {HomepageComponent} from './homepage/homepage.component';
-
+import { RequestedFormComponent } from './requested-form/requested-form.component';
 const routes: Routes = [
-  {path:'',component:HomepageComponent},
+  {path:'',component:RequestedFormComponent},
+  {path:'welcome',component:WelcomeComponent},
+  {path:'header',component:HomepageComponent},
   {path:'login',component:LoginComponent}
 ];
 
