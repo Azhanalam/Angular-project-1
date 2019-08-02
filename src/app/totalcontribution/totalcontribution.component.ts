5b30f4859e5edc507acbdfab33e26f49271949e8
prateek356827 <prateek.pandey2@wipro.com>
2019-08-02 20:11:34 +0530

    changed ui

--- src/app/totalcontribution/totalcontribution.component.ts
+++ src/app/totalcontribution/totalcontribution.component.ts
@@ -12,31 +12,31 @@ export class TotalcontributionComponent implements OnInit {
     ];
 
     date1:any  = new Date(new Date().setUTCDate(new Date().getDate()+3));
-    
+
     //  datenew= this.date1.getDate()+" "+ this.date1.toLocaleString('en-us', { month: 'long' }) + " " + this.date1.getFullYear();
      day = this.date1.getDate();
      month = this.date1.getMonth()+1;
      year = this.date1.getFullYear();
-   
+
      fullDate = this.day + '/' +this.month+ '/'+this.year;
       dateNow1 : String  = new Date().getDate() + " " + new Date().toLocaleString('en-us', { month: 'long' }) + " " + new Date().getFullYear();
-      
+
     // dateNow : String  = this.fullDate;
 
     // datenew1: String = this.datenew;
-    
+
 
   constructor(private router:Router) { }
 
   ngOnInit() {
-    
+
   }
 
  back()
  {
-this.router.navigate(["deduction"]);
+this.router.navigate(["cayecontribution"]);
 window.scrollTo(0, 0);
- } 
+ }
 
  acknowledge2(){
 this.router.navigate(["acknowledgement2"]);
--- src/app/view-caye-contribution/view-caye-contribution.component.ts
+++ src/app/view-caye-contribution/view-caye-contribution.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-view-caye-contribution',
@@ -11,38 +12,44 @@ export class ViewCayeContributionComponent implements OnInit {
   public No:any='';
   // public Yes:any='';
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
   minimize() {
-    
+
     if(!this.show)
    {
     this.show =!this.show;
     this.No='No';
    }
- else 
+ else
    {
     this.show =!this.show;
     this.No='No';
    }
-   
+
   }
 
   minimize1() {
-    
+
     if(!this.show1)
    {
     this.show1 =!this.show1;
     this.No='No';
    }
- else 
+ else
    {
     this.show1 =!this.show1;
     this.No='No';
    }
-   
+
   }
-  
+
+  back(){
+    this.router.navigate(["transactionhistorynew"]);
+    window.scrollTo(0, 0);
+
+  }
+
 }
